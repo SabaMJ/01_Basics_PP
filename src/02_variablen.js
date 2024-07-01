93d7a0c6124351bd46d831036b100b65c00e2d64
SabaMJ <rrrsaba@gmail.com>
2024-07-01 10:25:08 +0000

    vor und nachname ++

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,1 +1,11 @@
-console.log("hi, Saba ist hier");
+console.log("hi,  Wie geht es dir?");
+// console.log(Hi)
+/* Deklaration + Wertzuweisung I */
+let firstName; // Deklaration (Definition)
+//console.log(firstName); //Ausgabe
+firstName = "Saba";
+let familyName  ;
+familyName = "Moradmand Jalali"
+console.log(firstName + " "+ familyName); //Ausgabe
+
+
