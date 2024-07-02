21f6799f598536f6ec0f7ff4c30e961cdf15cff0
SabaMJ <rrrsaba@gmail.com>
2024-07-02 07:30:11 +0000

    02_05_kommit_constantUbung++

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -22,3 +22,31 @@ console.log("die Name ist : " + firstname);
 
 const test2 = "hi";
 console.log("inhalt : " + test2);
+
+
+const x = 3.14 ;
+console.log("symbol ist : " + x);
+
+// test = 2; // number
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+// test = true; // boolean
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+//let test; // Variable : Deklaration
+//test = "hi"; // Wertzuweisung (Initialisierung)
+//test = "hello"; // Überschreiben
+//console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test3 = "Hi!"; // Deklaration + Wertzuweisung gemeinsam !
+//test3 = "hello"; // Laufzeit-Fehler !! 
+console.log("Inhalt: " + test3); // Ausgabe 
+
