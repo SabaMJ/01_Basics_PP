1f401dfd02d27aebdfa3b3b0b9f0976c58950f3a
SabaMJ <rrrsaba@gmail.com>
2024-07-03 09:36:22 +0000

    06_01_ if ++

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,8 +1,13 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 30;
-const ageMark = 30;
+//const ageJohn = 30;
+//const ageMark = 30;
+
+// Alles muss man selber machen lassen! ...
+const prompt = require('prompt-sync')({sigint: true});
+let ageJohn = prompt("ageJohn?: ");
+let ageMark = prompt("ageMark?: ");
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
@@ -12,9 +17,35 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+//console.log("ageJohn: " + ageJohn);
+//console.log("ageMark: " + ageMark);
+ console.log("isJohnOlder: " + isJohnOlder);
+ console.log("isJohnEqual: " + isJohnEqual);
+ console.log("------------------");
 
+
+ /************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true) 
+// if(false)
+// if(isJohnOlder)
+/*if(ageJohn > ageMark)
+    {
+       console.log("John ist älter.");
+    }*/
+
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+if (ageJohn > ageMark)
+    {
+        console.log("John ist älter. "); //true 
+    }
+    else
+    {
+        console.log("Jahn ist jünger. "); //false 
+    }
