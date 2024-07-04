3d3a4adc513dc1b98639246cf54ee884f86b78e2
SabaMJ <rrrsaba@gmail.com>
2024-07-04 08:55:39 +0000

    08_02_kommit_Funktionen 02a

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -6,10 +6,40 @@
 // Funktionsaufruf | call
 // test();
 
-
+/*
 // Funktionsrumpf | callee
 // Funktionsdeklaration
 function test()
 {
     console.log("Hallo, Marius!");
 }
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Call
+ausgabeNamen();
+
+// Funktion
+function ausgabeNamen() {
+    // interne Variable | What happens in VEGAS ...
+    let firstName = "Sara";
+    console.log("Hallo, " + firstName + "!");
+}
+
+// console.log(firstName); // Fehler : scope!
+
+
+
+/*************Viktor ************/
+/*
+function ausgabeNamen() {
+    
+    let firstName =["Hubert", "sarah","Marius"]
+    for (let i = 0; i < firstName.length; i++) {
+        const names = firstName[i];
+        
+        console.log("Hallo,"+names + "!");
+    }
+}
+    */
