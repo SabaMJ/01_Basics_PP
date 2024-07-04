c07c0d69e5ce7cc9ca7208e4c5e09444270e1996
SabaMJ <rrrsaba@gmail.com>
2024-07-04 10:05:52 +0000

    08_04_Funktionen 03a ++

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -55,3 +55,18 @@ ausgabeNamen2("Saba");
 function ausgabeNamen2(firstName) { // Parameter
     console.log("Hallo, " + firstName + "!");    
 }
+
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenSRP("Max","Mütze");
+
+ausgabeNamenSRP("Max","Mütze");
+
+function ausgabeNamenSRP(firstName, familyName) {
+    console.log("Hallo, " + firstName + " " + familyName + "!");    
+}
+
