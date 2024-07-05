0cbd104e01716de35e7f56418953c5f2c69cd96a
SabaMJ <rrrsaba@gmail.com>
2024-07-05 08:47:37 +0000

    09_01_Rechner ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -11,16 +11,63 @@
 */
 
 
+// module: division a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+ function divide(a,b)
+ {
+	if (b != 0) 
+	{
+		return a/b;
+	}else
+	{
+		return "Teil durch 0 nicht möglich";
+	}
+	
+ }
+
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+	return  a * b;
+}
 
 
 
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+function subtract(a,b) {
+	return a - b;
+}
+
+
+// module: addition a + b |  test:
+//output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+
+function add(a,b)
+{
+	return a+b ;
+}
 
 
 
 // module: output | test:
 // output("hello");
 // output(2);
-function output(outputData) {
+function output(outputData) 
+{
 	console.log(outputData);
 }
-  
+ 
